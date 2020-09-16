f47ec8e199feacd87c0f3dc13a4ed3390acc389f
FURKAN AKGUN <fakgun@yukselproje.com.tr>
2020-09-17 01:13:38 +0300

    header and progress widgets done

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -75,7 +75,9 @@ class _HomeState extends State<Home> {
   }
 
   onTap(int pageIndex){
-    pageController.jumpToPage(pageIndex);
+    pageController.animateToPage(pageIndex,
+    duration: Duration(milliseconds: 300),
+    curve: Curves.easeInOut);
   }
 
   Scaffold buildAuthScreen() {
